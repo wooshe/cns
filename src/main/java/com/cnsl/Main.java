8d0a1558c277ee0ffa3c6d9bbdc61e378753f405
wooshe <j_g_g@list.ru>
2021-04-21 09:08:45 +0300

    news

---
+++ src/main/java/com/cnsl/Main.java
@@ -0,0 +1,17 @@
+package main.java.com.cnsl;
+
+public class Main {
+
+    public static void main(String[] args)
+
+    {
+        System.out.println("adsa asdas asfaf");
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
